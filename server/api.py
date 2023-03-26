6187ca1916a84de7dd79853fd64727e7240b317a
Tirthya Kamal Dasgupta <dasguptatirthyakamal@gmail.com>
2023-03-26 19:06:34 +0530

    Configured env file to allow origins based on environments

--- server/api.py
+++ server/api.py
@@ -1,16 +1,47 @@
+import logging
+import os
+import sys
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routes import portfolio_enquiry_submitter_router
+from dotenv import load_dotenv
 
 app = FastAPI(
     title="Portfolio enquiry submitter service",
     description="""
     Portfolio enquiry submitter service
     """,
-    version="0.0.3",
+    version="0.0.4",
     contact={
         "name": "Tirthya Kamal Dasgupta",
         "email": "dasguptatirthyakamal@gmail.com"
     }
+)
+
+load_dotenv()
+
+origins = []
+
+environment = os.environ.get("ENVIRONMENT")
+
+if not environment:
+    logging.critical("Deployment environment not found")
+
+    sys.exit()
+
+if environment == "DEV":
+    allow_origins = ["*"],
+elif environment == "STAGING":
+    allow_origins = ["*"]
+elif environment == "PROD":
+    allow_origins = ["https://portfolio-enquiry-submitter-service.vercel.app/"]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["POST"],
 )
 
 app.include_router(
